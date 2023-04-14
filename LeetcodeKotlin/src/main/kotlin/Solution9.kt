0e7cdab8bed6b1b5a8d3449ae0300b38d54009d6
Jon Etxeberria <121158787+RaistS@users.noreply.github.com>
2023-04-14 13:13:06 +0200

    Delete LeetcodeKotlin directory

--- LeetcodeKotlin/src/main/kotlin/Solution9.kt
+++
@@ -1,9 +0,0 @@
-class Solution9 {
-    fun isPalindrome(x: Int): Boolean {
-        // Convert the integer to a string
-        val xStr = x.toString()
-
-        // Check if the string is the same forwards and backwards
-        return xStr == StringBuilder(xStr).reverse().toString()
-    }
-}
